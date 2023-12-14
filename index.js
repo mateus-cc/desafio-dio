e2f04f75139ab8c7cce48f36ad9f1ece848a04c7
Mateus Costa <mateus-cc@outlook.com>
2023-12-13 21:55:45 -0300

    Correção do projeto

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 let nomePersonagem = "Mateus";
-let exp = 1500;
+let exp = 15000;
 
-switch (exp) {
+switch (true) {
     case (exp <= 1000):
-        console.log("A patente de " + nomePersonagem + "é ferro");
+        console.log("A patente de " + nomePersonagem + " é ferro");
         break;
-    case (exp >= 1001 || exp <= 2000):
-        console.log("A patente de " + nomePersonagem + "é bronze");
+    case (exp >= 1001 && exp <= 2000):
+        console.log("A patente de " + nomePersonagem + " é bronze");
         break;
-    case (exp >= 2001 || exp <= 5000):
-        console.log("A patente de " + nomePersonagem + "é prata");
+    case (exp >= 2001 && exp <= 5000):
+        console.log("A patente de " + nomePersonagem + " é prata");
         break;
-    case (exp >= 5001 || exp <= 7000):
-        console.log("A patente de " + nomePersonagem + "é ouro");
+    case (exp >= 5001 && exp <= 7000):
+        console.log("A patente de " + nomePersonagem + " é ouro");
         break;
-    case (exp >= 7001 || exp <= 8000):
-        console.log("A patente de " + nomePersonagem + "é platina");
+    case (exp >= 7001 && exp <= 8000):
+        console.log("A patente de " + nomePersonagem + " é platina");
         break;
-    case (exp >= 8001 || exp <= 9000):
-        console.log("A patente de " + nomePersonagem + "é ascendente");
+    case (exp >= 8001 && exp <= 9000):
+        console.log("A patente de " + nomePersonagem + " é ascendente");
         break;
-    case (exp > 9001 || exp < 10000):
-        console.log("A patente de " + nomePersonagem + "é imortal");
+    case (exp > 9001 && exp < 10000):
+        console.log("A patente de " + nomePersonagem + " é imortal");
         break;
     case (exp >= 10001):
-        console.log("A patente de " + nomePersonagem + "é radiante");
+        console.log("A patente de " + nomePersonagem + " é radiante");
         break;
 }
 
